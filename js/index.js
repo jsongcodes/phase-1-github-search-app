e4e63c773b251c2649111d5f3634852ee7625f97
Jasmin <ssongnari@gmail.com>
2022-04-23 14:17:15 -0400

    commit tree c9e938db666c45caa91d8ad43453f1041b809494

---
+++ js/index.js
@@ -0,0 +1,51 @@
+//grab form
+const form = document.getElementById("github-form")
+
+//add event listener to submit
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    //data we want to pass from the form
+    // event.target[0].value
+    fetch(`https://api.github.com/search/users?q=${event.target[0].value}`)
+        .then(response => response.json())
+        .then(response => {
+            //login, avatar_url, url
+            const userList = document.querySelector('#user-list')
+            const reposList = document.getElementById('repos-list')
+            reposList.innerHTML = ""
+            userList.innerHTML = ""
+            response.items.map(item => {
+                const li = document.createElement('li')
+                const h2 = document.createElement('h2')
+                h2.textContent = item.login
+
+                h2.addEventListener('click', e => showUserRepos(item.login, e))
+                const img = document.createElement('img')
+                img.src = item.avatar_url
+
+
+
+                li.append(h2, img)
+                userList.append(li)
+            })
+
+        })
+    form.reset()
+})
+
+function showUserRepos(username, e) {
+    const reposList = document.getElementById('repos-list')
+    reposList.List.innerHTML = ""
+    e.preventDefault()
+    fetch(`https://api.github.com/users/${username}/repos`)
+        .then(response => response.json())
+        .then(response => response.map(repo => {
+            const li = document.createElement('li')
+            const h1 = document.createElement('h1')
+            h1.textContent = repo.name
+            li.append(h1)
+            reposList.append(li)
+        }))
+}
+
+// const h1 = document.createElement('h1')
